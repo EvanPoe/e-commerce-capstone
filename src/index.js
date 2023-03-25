6e3fe18db3d86154421cfdcb725c34c3038d6b2d
Evan Poe <46730264+EvanPoe@users.noreply.github.com>
2023-03-24 19:14:10 -0500

    categories selectors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import { CategoriesProvider } from "./context/categories.context";
 import { CartProvider } from "./context/cart.context";
 import { Provider } from "react-redux";
 import { store } from './store/store';
@@ -17,11 +16,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-          <CategoriesProvider>
             <CartProvider>
               <App />
             </CartProvider>
-          </CategoriesProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
